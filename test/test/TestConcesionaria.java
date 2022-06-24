dc3c8e38ae834a0ab614e5f3838ca99258950cb5
mora avalos <moavalos@alumno.unlam.edu.ar>
2022-06-23 22:32:57 -0300

    start

---
+++ test/test/TestConcesionaria.java
@@ -0,0 +1,76 @@
+package test;
+
+import org.junit.Test;
+
+public class TestConcesionaria {
+
+	@Test
+	public void crearConcesionaria() {
+
+	}
+
+	@Test
+	public void crearCoche() {
+
+	}
+
+	@Test
+	public void queSePuedaAgregarUnAutoALaConcesionaria() {
+
+	}
+
+	@Test
+	public void queSePuedaEliminarUnAutoALaConcesionaria() {
+
+	}
+
+	@Test
+	public void queSePuedanMostrarTodosLosCochesDeLaConcesionaria() {
+
+	}
+
+	@Test
+	public void queNoSePuedaAgregarDosAutosConLaMismaPatente() {
+
+	}
+
+	@Test
+	public void queSePuedaBuscarUnAutoPorPatente() {
+
+	}
+
+	@Test
+	public void queSePuedaBuscarAutosPorMarca() {
+
+	}
+
+	@Test
+	public void queSePuedaOrdenarPorMarcaYModelo() {
+
+	}
+
+	@Test
+	public void queSePuedaOrdenarCochesPorPrecioYPatente() {
+
+	}
+
+	@Test
+	public void validarQueSeProduzcaUnaExcepcionSiLaPatenteEsInvalida() {
+
+	}
+
+	@Test
+	public void queAnteUnAutoInexistenteTermineConPatenteNoExisteException() {
+
+	}
+
+	@Test
+	public void queNoSePuedaBuscarUnAutoPorPatenteSiLaPatenteNoExiste() {
+
+	}
+
+	@Test
+	public void testAEleccion() {
+
+	}
+}
